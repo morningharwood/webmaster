3af113fed2e03ae020b9e0a1d509bfc41b76498a
Matthew Harwood <matthhar12@gmail.com>
2018-11-11 16:25:29 -0800

    Added Service workers.

--- morningharwood/apps/morningharwood/src/app/app.module.ts
+++ morningharwood/apps/morningharwood/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment.prod';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
   providers: [],
